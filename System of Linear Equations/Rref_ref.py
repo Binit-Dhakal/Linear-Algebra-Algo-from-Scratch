1d5447839849dad84c9e15c27aae5dd95afc366e
Binit-Dhakal <teslabuffet@gmail.com>
2021-10-26 01:31:03 +0545

    slight change in algo - 1

--- System of Linear Equations/Rref_ref.py
+++ System of Linear Equations/Rref_ref.py
@@ -48,21 +48,22 @@ class GaussJordan:
         
     def solve(self):
         R, C = 0, 0  # pivot row number, pivot column number
-        for i in range(self.row-1):
+        while (R < self.row and C < self.col):
             #handling the case where pivot has zero element
             k = R     #for iterating rows to find the non zero pivot
+
             while ( self.matrix[k][C] == 0 ):
-               
+    
                 # for checking if the search has reached at the final row and final column entry       
                 if (k == self.row - 1 and C == self.col - 1 ):
                     return self.matrix
-                
+
                 # for checking if the final row number has been reached: if it is reached we start our search to next column of initial row
                 if(k == self.row - 1):
                     C += 1
                     k = R
                     continue
-                
+
                 k += 1     
                 if (self.matrix[k][C] != 0):
                     self.matrix[[k-1,k],:] = self.matrix[[k,k-1],:]  #swap rows
@@ -76,7 +77,7 @@ class GaussJordan:
                 if (self.matrix[j][C] != 0 and j != R):
                     mul = self.matrix[j][C] / self.matrix[R][C]
                     self.matrix[j] = self.matrix[j] -  self.matrix[R] * mul
-
+                
             R += 1
             C += 1
             
